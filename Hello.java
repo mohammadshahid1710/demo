4e6cbec316ce605cacdebb35b74cedca469ffeda
mohammadshahid1710 <mohammadshahid1710@gmail.com>
2022-10-11 21:04:05 +0530

    First commit

---
+++ Hello.java
@@ -0,0 +1,15 @@
+package com.demo;
+
+public class Hello {
+	
+	public static void main(String[] args) {
+		System.out.println("Hello");
+	}
+	 
+
+}
+// public private protected default 
+//public any folder can access or class or subclass or package
+//private no one can access except its own class
+//protected its own package 
+//default access its own package b
